6eba9b2285e9f90877dc1316bd41599ddc647fe7
JonasWagenknecht <jw126@hdm-stuttgart.de>
2019-04-30 16:00:09 +0200

    Commit 2

--- SW_Praktikum_SS19_Gruppe5/src/de/hdm/SWPraktikum/client/ShoppinglistEditorEntry.java
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/SWPraktikum/client/ShoppinglistEditorEntry.java
@@ -42,7 +42,7 @@ public class ShoppinglistEditorEntry implements EntryPoint {
 	public void onModuleLoad() {
 		final Button sendButton = new Button("Send");
 		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
+		nameField.setText("Uffffffffffffffffff");
 		final Label errorLabel = new Label();
 
 		// We can add style names to widgets
