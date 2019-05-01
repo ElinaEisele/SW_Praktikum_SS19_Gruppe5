b1fc32692dd2c54f979d5302f2b8b498dedfdc6e
JonasWagenknecht <jw126@hdm-stuttgart.de>
2019-05-01 19:45:56 +0200

    test commit

--- SW_Praktikum_SS19_Gruppe5/src/de/hdm/SWPraktikum/client/ShoppinglistEditorEntry.java
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/SWPraktikum/client/ShoppinglistEditorEntry.java
@@ -24,10 +24,7 @@ import de.hdm.SWPraktikum.shared.ShoppinglistAdministrationAsync;
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class ShoppinglistEditorEntry implements EntryPoint {
-	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
+	
 	private static final String SERVER_ERROR = "An error occurred while "
 			+ "attempting to contact the server. Please check your network " + "connection and try again.";
 
