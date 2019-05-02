dd840497021db7f838ba94281ca4fcf5826113fc
TimBeutelspacher <tb125@hdm-stuttgart.de>
2019-05-02 12:06:16 +0200

    created Package .server.bo with classes:
    BusinessObject, NamedBusinessObject, Group, Listitem, Product, Retailer,
    Shoppinglist, User

---
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/server/bo/BusinessObject.java
@@ -0,0 +1,5 @@
+package de.hdm.softwarepraktikum.server.bo;
+
+public class BusinessObject {
+
+}
---
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/server/bo/User.java
@@ -0,0 +1,5 @@
+package de.hdm.softwarepraktikum.server.bo;
+
+public class User extends NamedBusinessObject{
+
+}
