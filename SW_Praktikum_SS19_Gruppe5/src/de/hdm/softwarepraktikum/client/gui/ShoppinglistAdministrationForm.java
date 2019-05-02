38447fdd8547c2335bb626f8359d92007faf0532
ElinaEisele <ee024@hdm-stuttgart.de>
2019-05-02 14:14:29 +0200

    adjusted structure in ShoppinglistAdministrationForm

--- SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/client/gui/ShoppinglistAdministrationForm.java
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/client/gui/ShoppinglistAdministrationForm.java
@@ -3,15 +3,18 @@ package de.hdm.softwarepraktikum.client.gui;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
-public class ShoppinglistAdministrationForm extends VerticalPanel{
+public class ShoppinglistAdministrationForm extends VerticalPanel {
 	
+	StartHeader startHeader = new StartHeader();
+	GroupHeader groupHeader = new GroupHeader();
+	ListHeader listHeader = new ListHeader();
+	EditorHeader editorHeader = new EditorHeader();
+
 	public void onLoad() {
 		super.onLoad();
-		
-		Button b1 = new Button("Click me 2");
-		
-		this.add(b1);
-		
+
+		this.add(listHeader);
+
 	}
 
 }
