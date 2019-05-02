4cb19165d97323a0425a4cb06370afaa1634660b
CarlaHofmann <ch143@hdm-stuttgart.de>
2019-05-02 14:10:44 +0200

    .server.db class GroupMapper: new constructor new protected constructor new method findAll() new method findByName()

--- SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/server/db/GroupMapper.java
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/server/db/GroupMapper.java
@@ -1,5 +1,96 @@
 package de.hdm.softwarepraktikum.server.db;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class GroupMapper {
+
+	/**
+	 * Speicherung der Instanz dieser Mapperklasse.
+	 */
+	private static GroupMapper groupMapper = null;
+
+	/**
+	 * Geschützter Konstruktor verhindert weitere Instanzierungen von UserMapper.
+	 */
+	protected GroupMapper() {
+	}
+
+	/**
+	 * Sicherstellung der Singleton-Eigenschaft der Mapperklasse.
+	 *
+	 * @return Gibt den Group Mapper zurück.
+	 */
+	public static GroupMapper groupMapper() {
+		if (groupMapper == null) {
+			groupMapper = new GroupMapper();
+		}
+
+		return groupMapper;
+	}
+
+	/**
+	 * Ausgabe einer Liste aller Gruppen.
+	 *
+	 * @return Gibt eine Liste aller Gruppen zurück.
+	 */
+	public ArrayList<Group> findAll() {
+
+		Connection con = DBConnection.connection();
+		ArrayList<Group> groups = new ArrayList<Group>();
+
+		try {
+
+			Statement stmt = con.createStatement();
+
+			ResultSet rs = stmt.executeQuery("SELECT ...");
+
+			while (rs.next()) {
+
+				Group group = new Group();
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		return groups;
+
+	}
+
+	/**
+	 * Gruppe mithilfe des Gruppennamen finden.
+	 * 
+	 * @param Übergabe des Namens einer Gruppe in Form eines Strings
+	 * @return Gruppe(n) mit dem entsprechenden Namen
+	 */
+	public ArrayList<Group> findByName (String name){
+		
+		Connection con = DBConnection.connection();
+		ArrayList<Group> groups = new ArrayList<Group>();
+
+		try {
+
+			Statement stmt = con.createStatement();
+
+			ResultSet rs = stmt.executeQuery("SELECT ...");
+
+			while (rs.next()) {
+
+				Group group = new Group();
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		return groups;
+		
+	}
 
 }
