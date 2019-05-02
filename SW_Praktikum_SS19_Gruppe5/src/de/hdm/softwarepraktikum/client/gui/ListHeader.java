1a509c7794a49b6611fb9700b38f8322b596c003
ElinaEisele <ee024@hdm-stuttgart.de>
2019-05-02 14:13:04 +0200

    new class ListHeader

---
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/client/gui/ListHeader.java
@@ -0,0 +1,24 @@
+package de.hdm.softwarepraktikum.client.gui;
+
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+
+public class ListHeader extends HorizontalPanel {
+
+	Label listHeaderLabel = new Label("Hier steht der Name der ausgewählten Liste");
+	Button addListItem = new Button("Eitrag hinzufügen Bild");
+	Button setUserRetailerAssignment = new Button("Nutzer Händler Zuweisung Bild");
+	Button deleteShoppinglist = new Button("Einkaufsliste löschen Bild");
+
+	public void onLoad() {
+		super.onLoad();
+
+		this.add(listHeaderLabel);
+		this.add(addListItem);
+		this.add(setUserRetailerAssignment);
+		this.add(deleteShoppinglist);
+
+	}
+
+}
