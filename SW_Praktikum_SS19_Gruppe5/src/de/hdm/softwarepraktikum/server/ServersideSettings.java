7b8cc1e00a6c62bad193f80f01c47b4c45447b64
CarlaHofmann <ch143@hdm-stuttgart.de>
2019-05-02 12:21:54 +0200

    ServersideSettings.java new LOGGER_NAME, new Logger log, new method getLogger()

--- SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/server/ServersideSettings.java
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/server/ServersideSettings.java
@@ -1,5 +1,35 @@
 package de.hdm.softwarepraktikum.server;
 
-public class ServersideSettings {
+import java.util.logging.Logger;
+
+import de.hdm.softwarepraktikum.shared.CommonSettings;
+
+/**
+ * <p>
+ * Klasse mit Eigenschaften und Diensten, die für alle Server-seitigen Klassen
+ * relevant sind.
+ * </p>
+ * <p>
+ * In ihrem aktuellen Entwicklungsstand bietet die Klasse eine rudimentäre
+ * Unterstützung der Logging-Funkionalität unter Java. Es wird ein
+ * applikationszentraler Logger realisiert, der mittels
+ * <code>ServerSideSettings.getLogger()</code> genutzt werden kann.
+ * </p>
+ * 
+ */
+
+public class ServersideSettings extends CommonSettings {
+	
+	private static final String LOGGER_NAME = "SharedShoppinglist Server";
+	private static final Logger log = Logger.getLogger(LOGGER_NAME);
+	
+	/**
+     * Auslesen des applikationsweiten zentralen Loggers.
+     * @return Server-Seitige Logger-Instanz 
+     */
+	
+	public static Logger getLogger() {
+	    return log;
+	}
 
 }
