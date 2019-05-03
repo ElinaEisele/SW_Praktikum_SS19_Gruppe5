84be4a93869c23f139a33a537d4a786559d8f20a
LeoniFriedrich <lf047@hdm-stuttgart.de>
2019-05-03 10:05:04 +0200

    protected constructor + basic structure findAll Method

--- SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/server/db/ShoppinglistMapper.java
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/server/db/ShoppinglistMapper.java
@@ -1,5 +1,79 @@
 package de.hdm.softwarepraktikum.server.db;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import de.hdm.softwarepraktikum.server.bo.Shoppinglist;
+import javafx.scene.Group;
+
+/**
+ * Mapper Klasse für </code>Shoppinglist</code> Objekte. Diese umfasst Methoden
+ * um Shoppinglist Objekte zu erstellen zu suchen, zu modifizieren und zu
+ * loeschen. Das Mapping funktioniert dabei bidirektional. Es koennen Objekte in
+ * DB-Strukturen und DB-Stukturen in Objekte umgewandelt werden.
+ * 
+ * @author LeoniFriedrich
+ */
+
 public class ShoppinglistMapper {
 
+	/**
+	 * Speicherung der Instanz dieser Mapper Klasse.
+	 */
+	private static ShoppinglistMapper shoppinglistMapper = null;
+
+	/**
+	 * Geschützter Konstruktor verhindert die Moeglichkeit, mit <code>new</code> neue
+	 * Instanzen dieser Klasse zu erzeugen.
+	 */
+
+	protected ShoppinglistMapper () {
+		
+	}
+	
+	/**
+	 * Sicherstellung der Singleton-Eigenschaft der Mapperklasse.
+	 * 
+	 * @return Gibt den Shoppinglist Mapper zurück. 
+	 */
+	
+	public static ShoppinglistMapper shoppinglistMapper() {
+		if (shoppinglistMapper == null) {
+			shoppinglistMapper = new ShoppinglistMapper();
+		}
+		return shoppinglistMapper;
+	}
+	
+	/**
+	 * Ausgabe einer Liste aller Shoppinglist Objekte 
+	 * 
+	 * @return gibt eine Liste aller Shoppinglist Objekte zurück. 
+	 */
+	public ArrayList<Shoppinglist> findAll() {
+
+		Connection con = DBConnection.connection();
+		ArrayList<Shoppinglist> shoppinglists = new ArrayList<Shoppinglist>();
+
+		try {
+
+			Statement stmt = con.createStatement();
+
+			ResultSet rs = stmt.executeQuery("SELECT ...");
+
+			while (rs.next()) {
+
+				Shoppinglist shoppinglist = new Shoppinglist();
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		return shoppinglists;
+
+	}
 }
