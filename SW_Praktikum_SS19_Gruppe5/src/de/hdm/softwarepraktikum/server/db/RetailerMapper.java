12f13a84d5c8bfa24b2f384f57a2d63c8813bdf1
LeoniFriedrich <lf047@hdm-stuttgart.de>
2019-05-03 11:51:39 +0200

    created basic structure of findAll and findById method

--- SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/server/db/RetailerMapper.java
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/server/db/RetailerMapper.java
@@ -1,5 +1,15 @@
 package de.hdm.softwarepraktikum.server.db;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import de.hdm.softwarepraktikum.server.bo.Retailer;
+import de.hdm.softwarepraktikum.server.bo.Shoppinglist;
+
+
 /**
  * Mapper Klasse für </code>Retailer</code> Objekte.
  * Diese umfasst  Methoden um RetailerMapper Objekte zu erstellen, zu suchen, zu  modifizieren und zu loeschen.
@@ -13,6 +23,82 @@ public class RetailerMapper {
 	/**
 	 * Speicherung der Instanz dieser Mapperklasse
 	 */
-	private static RetailerMapper retailermapper = null;
+	private static RetailerMapper retailerMapper = null;
+
+	/**
+	 * Geschuetzter Konstrukter verhindert weitere Instanzierungen von RetailerMapper Objekten.
+	 */
+	protected RetailerMapper() {
+	}
+	
+	/**
+	 * Sicherstellung der Singleton Eigenschaft der Mapperklasse 
+	 * 
+	 * @return Gibt den RetailerMapper zurueck. 
+	 */
+	
+	public static RetailerMapper retailerMapper() {
+		if (retailerMapper == null) {
+			retailerMapper = new RetailerMapper ();
+		}
+		return retailerMapper;
+	}
+	
+	/**
+	 * Ausgabe einer Liste aller Retailer Objekte 
+	 * 
+	 * @return gibt Liste aller Retailer Objekte zurück. 
+	 */
+	public ArrayList<Retailer> findAll() {
+
+		Connection con = DBConnection.connection();
+		ArrayList<Retailer> retailers = new ArrayList<Retailer>();
+
+		try {
+
+			Statement stmt = con.createStatement();
+
+			ResultSet rs = stmt.executeQuery("SELECT ...");
+
+			while (rs.next()) {
+
+				Retailer retailer = new Retailer();
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		return retailers;
+	}
+	
+	/**
+	 * Methode um Retailer mittels Id zu finden 
+	 * 
+	 * @param id: Die Id wird uebergeben. 
+	 * @return Der Retailer wird mit der jeweiligen id wird zurueckgegeben. 
+	 */
+	public Retailer findById(int id) {
+		Connection con = DBConnection.connection();
+
+		Retailer retailer = new Retailer();
+
+		try {
+			Statement stmt = con.createStatement();
+
+			ResultSet rs = stmt.executeQuery("SELECT ... ");
+			if (rs.next()) {
+
+			}
+		} catch (
+
+		SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		return retailer;
+	}
 
 }
