f411186845f01203735c09422f3a4f3a1059e1ff
ElinaEisele <ee024@hdm-stuttgart.de>
2019-05-03 12:33:05 +0200

    new class ClientSideSettings

---
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/client/ClientsideSettings.java
@@ -0,0 +1,14 @@
+package de.hdm.softwarepraktikum.client;
+
+import de.hdm.softwarepraktikum.shared.ShoppinglistAdministrationAsync;
+
+public class ClientsideSettings {
+
+	public static ShoppinglistAdministrationAsync getShoppinglistAdministration() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	}
+
+}
