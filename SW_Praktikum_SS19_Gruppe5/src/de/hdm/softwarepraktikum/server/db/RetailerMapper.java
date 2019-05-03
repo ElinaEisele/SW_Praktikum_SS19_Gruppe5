c75ae34b94cb5eba0f787f721cff935a5327909c
LeoniFriedrich <lf047@hdm-stuttgart.de>
2019-05-03 11:36:43 +0200

    created RetailerMapper class variable

--- SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/server/db/RetailerMapper.java
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/server/db/RetailerMapper.java
@@ -1,5 +1,18 @@
 package de.hdm.softwarepraktikum.server.db;
 
+/**
+ * Mapper Klasse für </code>Retailer</code> Objekte.
+ * Diese umfasst  Methoden um RetailerMapper Objekte zu erstellen, zu suchen, zu  modifizieren und zu loeschen.
+ * Das Mapping funktioniert dabei bidirektional. Es koennen Objekte in DB-Strukturen und DB-Stukturen in Objekte umgewandelt werden.
+ * @author Leoni Friedrich
+ *
+ */
+
 public class RetailerMapper {
+
+	/**
+	 * Speicherung der Instanz dieser Mapperklasse
+	 */
+	private static RetailerMapper retailermapper = null;
 
 }
