53ac3bb91dafe1d13b26c5ffde214e448ae62f6d
ElinaEisele <ee024@hdm-stuttgart.de>
2019-05-04 10:10:56 +0200

    added required methods for using them in the TreeViewModel class

--- SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/shared/ShoppinglistAdministration.java
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/shared/ShoppinglistAdministration.java
@@ -1,8 +1,14 @@
 package de.hdm.softwarepraktikum.shared;
 
 
+import java.util.ArrayList;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+import de.hdm.softwarepraktikum.shared.dummydata.Group;
+import de.hdm.softwarepraktikum.shared.dummydata.Shoppinglist;
 
 /**
  * The client-side stub for the RPC service.
@@ -10,4 +16,11 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 @RemoteServiceRelativePath("administration")
 public interface ShoppinglistAdministration extends RemoteService {
 	String greetServer(String name) throws IllegalArgumentException;
+	
+	Group getGroupById(int id);
+
+	ArrayList<Shoppinglist> getShoppinglistsOf(Group value);
+	
+	ArrayList<Group> getAllGroups();
+
 }
--- SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/shared/ShoppinglistAdministrationAsync.java
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/shared/ShoppinglistAdministrationAsync.java
@@ -1,10 +1,21 @@
 package de.hdm.softwarepraktikum.shared;
 
+import java.util.ArrayList;
+
 import com.google.gwt.user.client.rpc.AsyncCallback;
+
+import de.hdm.softwarepraktikum.shared.dummydata.Group;
+import de.hdm.softwarepraktikum.shared.dummydata.Shoppinglist;
 
 /**
  * The async counterpart of <code>GreetingService</code>.
  */
 public interface ShoppinglistAdministrationAsync {
 	void greetServer(String input, AsyncCallback<String> callback) throws IllegalArgumentException;
+
+	void getGroupById(int id, AsyncCallback<Group> callback);
+
+	void getShoppinglistsOf(Group value, AsyncCallback<ArrayList<Shoppinglist>> asyncCallback);
+
+	void getAllGroups(AsyncCallback<ArrayList<Group>> asyncCallback);
 }
