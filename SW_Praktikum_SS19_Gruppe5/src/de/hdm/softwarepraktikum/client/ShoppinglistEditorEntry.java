7dca4914d81cd49db602f4ced325c06553e87a17
ElinaEisele <ee024@hdm-stuttgart.de>
2019-05-05 18:58:02 +0200

    fixed bug in entry class

--- SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/client/ShoppinglistEditorEntry.java
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/client/ShoppinglistEditorEntry.java
@@ -35,83 +35,34 @@ import de.hdm.softwarepraktikum.client.gui.ShoppinglistCellTable;
  * 
  */
 public class ShoppinglistEditorEntry implements EntryPoint {
-	
+
 	ShoppinglistAdministrationAsync shoppinglistAdministration = null;
+
 		
 	private Header header = null;
 	private NavigatorPanel shoppinglistNavigator = null;
 	private ShoppinglistShowForm shoppinglistShowForm = null;
 	private GroupShowForm groupShowForm = null;
 	private Trailer trailer = null;
-	
-	private static class UserNavigator{
-		private final List<Group> groups = new ArrayList<Group>();
-		
-		public void addGroup(Group g) {
-			groups.add(g);
-		}
-		
-		public List<Group> getGroups(){
-			return groups;
-		}
-		
-	}
-	
-	private static class GroupNavigator{
-		private final List<Shoppinglist> shoppinglists = new ArrayList<Shoppinglist>();
-		
-		public Shoppinglist addShoppinglist(Shoppinglist s){
-			shoppinglists.add(s);
-			return s;
-		}
-		
-		public List<Shoppinglist> getShoppinglists(){
-			return shoppinglists;
-		}
-		
-	}
-	
+
 	/**
 	   * The model that defines the nodes in the tree.
 	   */
 	  private static class CustomTreeModel implements TreeViewModel {
-		  
-		  private List<Group> groups1 = null;
-		  
-		  private final SingleSelectionModel<String> selectionModel = new SingleSelectionModel<String>();		  
-		  
-		  public CustomTreeModel() {
-			  groups1 = new ArrayList<Group>();
-			  
-			  Group g1 = new Group("Gruppe 1", 0, new Date());
-			  Group g2 = new Group("Gruppe 2", 3, new Date());
-			  groups1.add(g1);
-			  groups1.add(g2);
-			  
-			  Shoppinglist s1 = new Shoppinglist("Einkaufsliste 1", 1, new Date());
-			  Shoppinglist s2 = new Shoppinglist("Einkaufsliste 2", 2, new Date());
-			  Shoppinglist s3 = new Shoppinglist("Einkaufsliste 1", 4, new Date());
-			  
-			  g1.addShoppinglist(s1);
-			  g1.addShoppinglist(s2);
-			  g2.addShoppinglist(s3);
-			  
-		  }
 
 	    /**
 	     * Get the {@link NodeInfo} that provides the children of the specified
 	     * value.
 	     */
 	    public <T> NodeInfo<?> getNodeInfo(T value) {
-	    	if (value == null) {
-	    		/*
-	  	       * Create some data in a data provider. Use the parent value as a prefix
-	  	       * for the next level.
-	  	       */
-	  	      ListDataProvider<UserNavigator> dataProvider = new ListDataProvider<ShoppinglistEditorEntry.UserNavigator>(groups1);
-
-	    		}
-	      
+	      /*
+	       * Create some data in a data provider. Use the parent value as a prefix
+	       * for the next level.
+	       */
+	      ListDataProvider<String> dataProvider = new ListDataProvider<String>();
+	      for (int i = 0; i < 2; i++) {
+	        dataProvider.getList().add(value+"." + String.valueOf(i));
+	      }
 
 	      // Return a node info that pairs the data with a cell.
 	      return new DefaultNodeInfo<String>(dataProvider, new TextCell());
@@ -126,37 +77,26 @@ public class ShoppinglistEditorEntry implements EntryPoint {
 	      return value.toString().length() > 10;
 	    }
 	  }
-	
-	
-	/**
-	 * Da diese Klasse die Implementierung des Interrface <code>EntryPoint</code>
-	 * sicherstellt, wird die Methode <code>public void onModuleLoad()</code> benoetigt.
-	 * Diese ist wie die <code>main()</code>-Methode in Java-Applikationen
-	 * fuer GWT zu verstehen.
-	 */
-	@Override
-	public void onModuleLoad() {
-		
-		
-		
-	    TreeViewModel model = new CustomTreeModel();
-	    
-	    CellTree cellTree = new CellTree(model, "Gruppe ");
-				
-		header = new Header();
-		shoppinglistShowForm = new ShoppinglistShowForm();
-		groupShowForm = new GroupShowForm();
-		trailer = new Trailer();
-		
-						
-		RootPanel.get("Header").add(header);
-		RootPanel.get("Navigator").add(cellTree);
-		RootPanel.get("Details").add(shoppinglistShowForm);
-		RootPanel.get("Trailer").add(trailer);
-	
-	
-		
-	}
-	
+	  
+	  @Override
+		public void onModuleLoad() {
 
+		    TreeViewModel model = new CustomTreeModel();
+
+		    CellTree cellTree = new CellTree(model, "Gruppe 1");
+
+			header = new Header();
+			
+			shoppinglistShowForm = new ShoppinglistShowForm();
+			groupShowForm = new GroupShowForm();
+			trailer = new Trailer();
+
+
+			RootPanel.get("Header").add(header);
+			RootPanel.get("Navigator").add(cellTree);
+			RootPanel.get("Details").add(shoppinglistShowForm);
+			RootPanel.get("Trailer").add(trailer);
+			
+	  }
+	 
 }
