d92439b8003616d6a67938ae910dc21b3a5d5961
FelixRapp <fr056@hdm-stuttgart.de>
2019-05-06 11:31:57 +0200

    Added Methods to Listitem, Product, Retailer

--- SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/shared/bo/Product.java
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/shared/bo/Product.java
@@ -1,5 +1,11 @@
 package de.hdm.softwarepraktikum.shared.bo;
 
 public class Product extends NamedBusinessObject{
+	
+	private static final long serialVersionUID = 1L;
+	
+	public Product(String productname) {
+		super(productname);
+	}
 
 }
--- SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/shared/bo/Retailer.java
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/shared/bo/Retailer.java
@@ -1,5 +1,10 @@
 package de.hdm.softwarepraktikum.shared.bo;
 
 public class Retailer extends NamedBusinessObject{
-
+	
+	private static final long serialVersionUID = 1L;
+	
+	public Retailer(String retailername) {
+		super(retailername);
+	}
 }
