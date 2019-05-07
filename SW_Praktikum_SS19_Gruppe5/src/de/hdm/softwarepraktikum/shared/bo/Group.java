368f5d40f2694fb8d4c25e00f183afbd9a907f92
FelixRapp <fr056@hdm-stuttgart.de>
2019-05-07 12:55:15 +0200

    Added Methods to shared.bo

--- SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/shared/bo/Group.java
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/shared/bo/Group.java
@@ -1,6 +1,44 @@
 package de.hdm.softwarepraktikum.shared.bo;
 
-public class Group extends NamedBusinessObject{
+import java.util.ArrayList;
+import java.util.Vector;
 
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+/**
+ * Realisierung der Klasse Group, welche eine Gruppe darstellt, welche aus mehreren Mitgliedern (User)
+ * und Einkaufslisten (Shoppinglist) bestehen kann.
+ * 
+ * @author Felix Rapp
+ */
+
+public class Group extends NamedBusinessObject implements IsSerializable{
+
+	private ArrayList<User> users;
+	private ArrayList<Shoppinglist> shoppinglists;
+	private ArrayList<Listitem> standardListitem;
 	
+	public Group(String groupname) {
+		this.setName(groupname);
+		users.add(this);
+	}
+	
+	public ArrayList<User> getUsers() {
+		return users;
+	}
+	public void setUser(ArrayList<User> users) {
+		this.users = users;
+	}
+	public ArrayList<Shoppinglist> getShoppinglists() {
+		return shoppinglists;
+	}
+	public void setShoppinglists(ArrayList<Shoppinglist> shoppinglists) {
+		this.shoppinglists = shoppinglists;
+	}
+	public ArrayList<Listitem> getStandardListitem() {
+		return standardListitem;
+	}
+	public void setStandardListitem(ArrayList<Listitem> standardListitem) {
+		this.standardListitem = standardListitem;
+	}
 }
