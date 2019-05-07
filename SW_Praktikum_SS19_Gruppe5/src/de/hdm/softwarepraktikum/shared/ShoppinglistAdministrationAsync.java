2cc7e8356c88ed4cfbd956505e9c4769699b7028
FelixRapp <fr056@hdm-stuttgart.de>
2019-05-07 16:10:30 +0200

    Added Methods to ShoppinglisAdministrationAsync

--- SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/shared/ShoppinglistAdministrationAsync.java
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/shared/ShoppinglistAdministrationAsync.java
@@ -26,12 +26,25 @@ public interface ShoppinglistAdministrationAsync {
 	
 	void createShoppinglist(Group group, String name, AsyncCallback<Shoppinglist> callback) throws IllegalArgumentException;
 	
-	void createListitem(Shoppinglist shoppinglist, )
-	
 	
 	void getGroupById(int id, AsyncCallback<Group> callback);
 
 	void getShoppinglistsOf(Group value, AsyncCallback<ArrayList<Shoppinglist>> asyncCallback);
 
+	void getUsersOf(Group group, AsyncCallback<ArrayList<User>> callback) throws IllegalArgumentException;
 	
+	void getUsersOf(int groupId, AsyncCallback<ArrayList<User>> callback ) throws IllegalArgumentException;
+	
+	void getUserById(int userId, AsyncCallback<User> callback) throws IllegalArgumentException;
+	
+	void getUsersByName(String name, AsyncCallback<ArrayList<User>>callback) throws IllegalArgumentException;
+	
+	void getUserByMail(String mail, AsyncCallback<User> callback) throws IllegalArgumentException;
+	
+	void getShoppinglistsByName(String name, AsyncCallback<ArrayList<Shoppinglist>> callback) throws IllegalArgumentException;
+	
+	void getShoppinglistById(int shoppinglistId, AsyncCallback<Shoppinglist> callback) throws IllegalArgumentException;
+	
+	void getListitemsOf(Shoppinglist shoppinglist, String productname, AsyncCallback<ArrayList<Listitem>>callback) throws IllegalArgumentException;
+
 }
