148b1f020fa7bd84f9f7d60e5c8a600857672b9f
FelixRapp <fr056@hdm-stuttgart.de>
2019-05-07 15:41:52 +0200

    Added Methods to Listitem, Retailer, Group and User

--- SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/shared/bo/Retailer.java
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/shared/bo/Retailer.java
@@ -2,11 +2,21 @@ package de.hdm.softwarepraktikum.shared.bo;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
 
+/**
+ * Realisierung der Klasse Retailer, welche einen Händler darstellt. Dieser ist für alle Nutzer
+ * des Systems sichtbar und kann einzelnen Einträgen hinzugefügt werden.
+ * 
+ * @author Felix Rapp
+ */
+
 public class Retailer extends NamedBusinessObject implements IsSerializable{
 	
 	private static final long serialVersionUID = 1L;
 	
+	/**
+	 * Konstruktor zum Setzen des Namens des Händlers.
+	 */
 	public Retailer(String retailername) {
-		this.setName(retailername);
+		super(retailername);
 	}
 }
