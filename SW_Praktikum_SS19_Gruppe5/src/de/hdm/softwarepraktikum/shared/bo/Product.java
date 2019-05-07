f2771b9d3d97e7c631db9760669a48c1ae1536e1
TimBeutelspacher <tb125@hdm-stuttgart.de>
2019-05-07 14:43:18 +0200

    added comments to product

--- SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/shared/bo/Product.java
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/shared/bo/Product.java
@@ -6,22 +6,35 @@ import com.google.gwt.user.client.rpc.IsSerializable;
  * Realisierung der Klasse Product, welche ein Produkt darstellt, welches einem Eintrag (Listitem)
  * einer Shoppingliste zugeordnet ist.
  * 
- * @author Felix Rapp
+ * @author Felix Rapp, TimBeutelspacher
  */
 
 public class Product extends NamedBusinessObject implements IsSerializable{
 	
 	private static final long serialVersionUID = 1L;
+	
+	/*
+	 * Fremdschlüsselbeziehung zum zugeordneten Listitem-Objekt
+	 */
 	private int listitemID;
 	
+	/*
+	 * Konstruktor, in welchem der Name gesetzt wird.
+	 */
 	public Product(String productname) {
-		this.setName(productname);
+		super(productname);
 	}
 
+	/*
+	 * Ausgeben der ID, des zugeordneten Listitem-Objekts
+	 */
 	public int getListitemID() {
 		return listitemID;
 	}
 
+	/*
+	 *setzen der ID, des zugeordneten Listitem-Objekts 
+	 */
 	public void setListitemID(int listitemID) {
 		this.listitemID = listitemID;
 	}
