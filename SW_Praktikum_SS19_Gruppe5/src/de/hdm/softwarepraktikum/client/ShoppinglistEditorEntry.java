289717524b9b48ecc8f1c15314e997a50eb6a167
JonasWagenknecht <jw126@hdm-stuttgart.de>
2019-05-08 16:41:56 +0200

    added flexbox making the gui responsive

--- SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/client/ShoppinglistEditorEntry.java
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/client/ShoppinglistEditorEntry.java
@@ -29,7 +29,8 @@ import de.hdm.softwarepraktikum.client.gui.Header;
 import de.hdm.softwarepraktikum.client.gui.ShoppinglistCellTable;
 
 /**
- * Entry-Point Klasse des Projekts <b>SW_Praktikum_SS19_Gruppe5</b> für den Editor.
+ * Entry-Point Klasse des Projekts <b>SW_Praktikum_SS19_Gruppe5</b> für den
+ * Editor.
  * 
  * @author ElinaEisele , JonasWagenknecht
  * 
@@ -38,7 +39,6 @@ public class ShoppinglistEditorEntry implements EntryPoint {
 
 	ShoppinglistAdministrationAsync shoppinglistAdministration = null;
 
-		
 	private Header header = null;
 	private NavigatorPanel shoppinglistNavigator = null;
 	private ShoppinglistShowForm shoppinglistShowForm = null;
@@ -46,57 +46,67 @@ public class ShoppinglistEditorEntry implements EntryPoint {
 	private Trailer trailer = null;
 
 	/**
-	   * The model that defines the nodes in the tree.
-	   */
-	  private static class CustomTreeModel implements TreeViewModel {
+	 * The model that defines the nodes in the tree.
+	 */
+	private static class CustomTreeModel implements TreeViewModel {
 
-	    /**
-	     * Get the {@link NodeInfo} that provides the children of the specified
-	     * value.
-	     */
-	    public <T> NodeInfo<?> getNodeInfo(T value) {
-	      /*
-	       * Create some data in a data provider. Use the parent value as a prefix
-	       * for the next level.
-	       */
-	      ListDataProvider<String> dataProvider = new ListDataProvider<String>();
-	      for (int i = 0; i < 10; i++) {
-	        dataProvider.getList().add(value+"." + String.valueOf(i));
-	      }
+		/**
+		 * Get the {@link NodeInfo} that provides the children of the specified value.
+		 */
+		public <T> NodeInfo<?> getNodeInfo(T value) {
+			/*
+			 * Create some data in a data provider. Use the parent value as a prefix for the
+			 * next level.
+			 */
+			ListDataProvider<String> dataProvider = new ListDataProvider<String>();
+			for (int i = 0; i < 10; i++) {
+				dataProvider.getList().add(value + "." + String.valueOf(i));
+			}
 
-	      // Return a node info that pairs the data with a cell.
-	      return new DefaultNodeInfo<String>(dataProvider, new TextCell());
-	    }
+			// Return a node info that pairs the data with a cell.
+			return new DefaultNodeInfo<String>(dataProvider, new TextCell());
+		}
 
-	    /**
-	     * Check if the specified value represents a leaf node. Leaf nodes cannot be
-	     * opened.
-	     */
-	    public boolean isLeaf(Object value) {
-	      // The maximum length of a value is ten characters.
-	      return value.toString().length() > 10;
-	    }
-	  }
-	  
-	  @Override
-		public void onModuleLoad() {
+		/**
+		 * Check if the specified value represents a leaf node. Leaf nodes cannot be
+		 * opened.
+		 */
+		public boolean isLeaf(Object value) {
+			// The maximum length of a value is ten characters.
+			return value.toString().length() > 10;
+		}
+	}
 
-		    TreeViewModel model = new CustomTreeModel();
+	@Override
+	public void onModuleLoad() {
 
-		    CellTree cellTree = new CellTree(model, "Gruppe 1");
+		TreeViewModel model = new CustomTreeModel();
 
-			header = new Header();
-			
-			shoppinglistShowForm = new ShoppinglistShowForm();
-			groupShowForm = new GroupShowForm();
-			trailer = new Trailer();
+		CellTree cellTree = new CellTree(model, "Gruppe 1");
 
+		header = new Header();
 
-			RootPanel.get("Header").add(header);
-			RootPanel.get("Navigator").add(cellTree);
-			RootPanel.get("Details").add(groupShowForm);
-			RootPanel.get("Trailer").add(trailer);
-			
-	  }
-	 
+		shoppinglistShowForm = new ShoppinglistShowForm();
+		groupShowForm = new GroupShowForm();
+		trailer = new Trailer();
+
+		
+		Button b1 = new Button("Editor");
+		Button b2 = new Button("Report");
+		Button b3 = new Button("Abmelden");
+		
+		b1.setStyleName("HeaderButton");
+		b2.setStyleName("HeaderButton");
+		b3.setStyleName("HeaderButton");
+
+		RootPanel.get("Editor").add(b1);
+		RootPanel.get("Report").add(b2);
+		RootPanel.get("Logout").add(b3);
+
+		RootPanel.get("aside").add(cellTree);
+		RootPanel.get("main").add(groupShowForm);
+		RootPanel.get("footer").add(trailer);
+
+	}
+
 }
