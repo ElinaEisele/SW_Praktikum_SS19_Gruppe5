02874a9268ed4631ab0c588e0aacb6d5b08ba208
CarlaHofmann <ch143@hdm-stuttgart.de>
2019-05-13 19:40:49 +0200

    new class DBZugriff: test DB Connection

---
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/server/db/DBZugriff.java
@@ -0,0 +1,32 @@
+package de.hdm.softwarepraktikum.server.db;
+
+import java.sql.*;
+
+public class DBZugriff {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Connection con;
+		Statement stmt;
+		ResultSet rs;
+		
+		try {
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test?user=root&password=");
+			stmt = con.createStatement();
+			rs = stmt.executeQuery("SELECT * from users");
+			
+			while(rs.next()){
+				System.out.println(rs.getString("name"));
+				
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
