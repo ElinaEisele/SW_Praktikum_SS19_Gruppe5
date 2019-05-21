577ae1a254be87b43a66d45cfb10aa32d5fe4942
JonasWagenknecht <jw126@hdm-stuttgart.de>
2019-05-21 20:04:34 +0200

    Add missing ClickHandlers to buttons

--- SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/client/gui/ShoppinglistShowForm.java
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/client/gui/ShoppinglistShowForm.java
@@ -5,40 +5,36 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 import de.hdm.softwarepraktikum.shared.bo.Shoppinglist;
 
 /**
- * Klasse zur Uebersicht einer Shoppingliste mit einem Header inklusiv
- * Buttons zum bearbeiten der Shoppingliste und mit der Anzeige des Inhalt der
- * Shoppingliste.
+ * Klasse zur Uebersicht einer Shoppingliste mit einem Header und mit der
+ * Anzeige des Inhalt der Shoppingliste.
  * 
  * @author ElinaEisele, JonasWagenkencht
  */
 
 public class ShoppinglistShowForm extends VerticalPanel {
-	
-	ShoppinglistHeader shoppinglistHeader = null;
-	ShoppinglistContent shoppinglistContent = null;
-	
-	
+
+	private ShoppinglistHeader shoppinglistHeader = null;
+	private ShoppinglistContent shoppinglistContent = null;
+
+	private Shoppinglist selectedShoppinglist;
+
 	public void onLoad() {
 		super.onLoad();
-		
+
 		shoppinglistHeader = new ShoppinglistHeader();
 		shoppinglistContent = new ShoppinglistContent();
-		
+
 		shoppinglistHeader.setStylePrimaryName("shoppinglistHeader");
 		shoppinglistContent.setStylePrimaryName("shoppinglistContent");
-		
-		shoppinglistHeader.setSho
-		
+
 		this.add(shoppinglistHeader);
 		this.add(shoppinglistContent);
 
-
 	}
 
-
 	public void setSelected(Shoppinglist s) {
-		// TODO Auto-generated method stub
-		
+		selectedShoppinglist = s;
+
 	}
 
 }
