cc9f30083399a019caba5bee505ddd176b314caf
TimBeutelspacher <tb125@hdm-stuttgart.de>
2019-05-21 09:15:13 +0200

    translated comments in class Fieldverifier

--- SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/shared/FieldVerifier.java
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/shared/FieldVerifier.java
@@ -2,36 +2,26 @@ package de.hdm.softwarepraktikum.shared;
 
 /**
  * <p>
- * FieldVerifier validates that the name the user enters is valid.
+ * Die Klasse <code>FieldVerifier</code> überprüft den eigegebenen Namen des Users auf dessen Gültigkeit.
  * </p>
  * <p>
- * This class is in the <code>shared</code> package because we use it in both
- * the client code and on the server. On the client, we verify that the name is
- * valid before sending an RPC request so the user doesn't have to wait for a
- * network round trip to get feedback. On the server, we verify that the name is
- * correct to ensure that the input is correct regardless of where the RPC
- * originates.
+ * Diese Klasse befindet sich im <code>shared</code> -package, da es sowohl auf Client- als auch 
+ * auf ServerSeite genutzt wird. Auf der Client-seite wird der Name überprüft, bevor dieser in einem 
+ * RPC-Request gesendet wird. Somit muss der Nutzer nicht warten bis der Server antwortet.
+ * Auf der Server-Seite wird überprüft, ob der Name gültig ist um Fehlern vorzubeugen.
  * </p>
- * <p>
- * When creating a class that is used on both the client and the server, be sure
- * that all code is translatable and does not use native JavaScript. Code that
- * is not translatable (such as code that interacts with a database or the file
- * system) cannot be compiled into client-side JavaScript. Code that uses native
- * JavaScript (such as Widgets) cannot be run on the server.
- * </p>
+ * @author TimBeutelspacher
  */
+
 public class FieldVerifier {
 
 	/**
-	 * Verifies that the specified name is valid for our service.
+	 * Prüft den übergebenen Namen auf dessen Gültigkeit.
+	 * Hierbei sind die Bedinungen, dass der Übergebene String nicht null ist UND 
+	 * eine Länge von mindestens 4 Zeichen besitzt.
 	 * 
-	 * In this example, we only require that the name is at least four
-	 * characters. In your application, you can use more complex checks to ensure
-	 * that usernames, passwords, email addresses, URLs, and other fields have the
-	 * proper syntax.
-	 * 
-	 * @param name the name to validate
-	 * @return true if valid, false if invalid
+	 * @param name ist der zuüberprüfende Name
+	 * @return true oder false, je nach dem ob der String den Bedingungen entspricht.
 	 */
 	public static boolean isValidName(String name) {
 		if (name == null) {
