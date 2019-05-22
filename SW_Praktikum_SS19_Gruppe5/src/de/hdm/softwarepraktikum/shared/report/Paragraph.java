ede337f20336709a59138e9633509f8243eae91a
FelixRapp <fr056@hdm-stuttgart.de>
2019-05-22 13:13:35 +0200

    Added body to class Paragraph in shared.report

--- SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/shared/report/Paragraph.java
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/shared/report/Paragraph.java
@@ -1,5 +1,19 @@
 package de.hdm.softwarepraktikum.shared.report;
 
-public class Paragraph {
+import java.io.Serializable;
+
+/**
+ * Die Klasse Paragraph dient zum speichern von Text. Mithilfe des ReportWriters lässt 
+ * dieser Text sich später in verschiedene Zielformate konvertieren.
+ * 
+ * Paragraph ist Serializable, so das Objekte dieser
+ * Klasse durch das Netzwerk übertragbar sind.
+ * 
+ * @author FelixRapp
+ */
+public abstract class Paragraph implements Serializable {
+
+  private static final long serialVersionUID = 1L;
 
 }
+
