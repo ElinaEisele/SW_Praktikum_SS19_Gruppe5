705ca61598efcf21f95142e60af706ddd3e6523a
FelixRapp <fr056@hdm-stuttgart.de>
2019-05-22 19:36:47 +0200

    Added Body to SimpleParagraph

--- SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/shared/report/SimpleParagraph.java
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/shared/report/SimpleParagraph.java
@@ -1,5 +1,60 @@
 package de.hdm.softwarepraktikum.shared.report;
 
-public class SimpleParagraph extends Paragraph{
+import java.io.Serializable;
 
+/**
+ * Stellt einen einzelnen Absatz dar, dessen Inhalt als String abgespeichert wird.
+ * 
+ * @author FelixRapp
+ */
+public class SimpleParagraph extends Paragraph implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * Inhalt des Absatzes.
+   */
+  private String text = "";
+
+  /**
+   * Es wird ein No-Argument Konstruktor benötigt. Da es einen Konstruktor mit Argumenten
+   * gibt muss der No-Argument Konstruktor somit auch angegeben werden.
+   */
+  public SimpleParagraph() {
+  }
+
+  /**
+   * Konstruktur, der die Möglichkeit bietet bei Instanziierung, einen Wert festzulegen.
+   * 
+   * @param value Inhalt des Absatzes
+   */
+  public SimpleParagraph(String value) {
+    this.text = value;
+  }
+
+  /**
+   * Auslesen des Inhalts.
+   * 
+   * @return Inhalt als String
+   */
+  public String getText() {
+    return this.text;
+  }
+
+  /**
+   * Ändern des Inhalts.
+   * 
+   * @param text der neue Inhalt des Absatzes.
+   */
+  public void setText(String text) {
+    this.text = text;
+  }
+
+  /**
+   * Das Simple-Paragraph-Objekt wird als String ausgegeben.
+   */
+  @Override
+  public String toString() {
+    return this.text;
+  }
 }
