99b9051b4910c31a6747fcd4d01e2eb04e9672d6
LeoniFriedrich <lf047@hdm-stuttgart.de>
2019-05-29 12:43:04 +0200

    updated comment

--- SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/client/gui/report/ShoppinglistAdministrationReport.java
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/client/gui/report/ShoppinglistAdministrationReport.java
@@ -1,5 +1,11 @@
 package de.hdm.softwarepraktikum.client.gui.report;
 
+/**
+ * Diese Klasse stellt die Hauptform des ReportGenerator Clients dar. Hier werden alle relevanten HTML-Layout Elemente
+ * zu einer Form zusammengefuehrt.
+ * 
+ * @author LeoniFriedrich
+ */
 public class ShoppinglistAdministrationReport {
 
 }
