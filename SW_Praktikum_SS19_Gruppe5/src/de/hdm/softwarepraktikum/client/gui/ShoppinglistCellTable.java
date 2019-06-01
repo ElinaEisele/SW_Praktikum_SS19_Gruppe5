d3ba66dac2aa49530de1491d0ffaf834deaa7dbd
JonasWagenknecht <jw126@hdm-stuttgart.de>
2019-06-01 15:45:55 +0200

    Add Columns to CellTable widget

--- SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/client/gui/ShoppinglistCellTable.java
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/client/gui/ShoppinglistCellTable.java
@@ -22,7 +22,6 @@ import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.view.client.DefaultSelectionEventManager;
 import com.google.gwt.view.client.ListDataProvider;
@@ -32,7 +31,6 @@ import de.hdm.softwarepraktikum.client.ClientsideSettings;
 import de.hdm.softwarepraktikum.shared.ShoppinglistAdministrationAsync;
 import de.hdm.softwarepraktikum.shared.bo.Listitem;
 import de.hdm.softwarepraktikum.shared.bo.ListitemUnit;
-import de.hdm.softwarepraktikum.shared.bo.Product;
 import de.hdm.softwarepraktikum.shared.bo.Retailer;
 import de.hdm.softwarepraktikum.shared.bo.Shoppinglist;
 
@@ -43,13 +41,47 @@ import de.hdm.softwarepraktikum.shared.bo.Shoppinglist;
  */
 
 public class ShoppinglistCellTable extends VerticalPanel {
-	private String productName = new String();
-	private String unitName = new String();
-	private Label retailerName = new Label("Uff");
-	private Label retailerName2 = new Label("Uff2");
 
-	ArrayList<Product> products;
-	ArrayList<Retailer> retailer;
+	private Label productName = new Label("Fehler beim Laden");
+	private Float amount = new Float(0.0F);
+	private Label unitName = new Label("Fehler beim Laden");
+	private Label retailerName = new Label("Fehler beim Laden");
+
+	Column<Listitem, String> productNameToDisplay = new Column<Listitem, String>(new TextCell()) {
+		@Override
+		public String getValue(Listitem object) {
+
+			return productName.getText();
+
+		}
+	};
+
+	NumberCell amountCell = new NumberCell();
+	Column<Listitem, Number> amountToDisplay = new Column<Listitem, Number>(amountCell) {
+		@Override
+		public Number getValue(Listitem object) {
+			return amount;
+		}
+	};
+
+	Column<Listitem, String> unitNameToDisplay = new Column<Listitem, String>(new TextCell()) {
+		@Override
+		public String getValue(Listitem object) {
+
+			return unitName.getText();
+
+		}
+	};
+
+	Column<Listitem, String> retailerNameToDisplay = new Column<Listitem, String>(new TextCell()) {
+		@Override
+		public String getValue(Listitem object) {
+
+			return retailerName.getText();
+
+		}
+	};
+
 	ArrayList<Listitem> listitems;
 
 	ShoppinglistContent shoppinglistContent;
@@ -63,8 +95,6 @@ public class ShoppinglistCellTable extends VerticalPanel {
 	private ShoppinglistAdministrationAsync shoppinglistAdministration = null;
 	private Shoppinglist selectedShoppinglist;
 	private Map<Shoppinglist, ListDataProvider<Listitem>> listitemDataProviders = null;
-
-	// muss noch Ã¼ber Navigator gesetzt werden
 
 	public Shoppinglist getSelectedShoppinglist() {
 		return selectedShoppinglist;
@@ -94,9 +124,11 @@ public class ShoppinglistCellTable extends VerticalPanel {
 		shoppinglistContent = new ShoppinglistContent();
 
 		listitems = new ArrayList<>();
-		products = new ArrayList<>();
-		retailer = new ArrayList<>();
 
+		/**
+		 * Get Listitems from Shoppinglist and add to ArrayList
+		 * 
+		 */
 		shoppinglistAdministration.getListitemsOf(selectedShoppinglist, new AsyncCallback<ArrayList<Listitem>>() {
 
 			@Override
@@ -115,27 +147,13 @@ public class ShoppinglistCellTable extends VerticalPanel {
 		 * Testdata
 		 * 
 		 */
-		Product product1 = new Product("Tomate");
-		Product product2 = new Product("Apfel");
-		products.add(product1);
-		products.add(product2);
-
-		Retailer retailer1 = new Retailer("Aldi");
-		Retailer retailer2 = new Retailer("Kaufland");
-		retailer.add(retailer1);
-		retailer.add(retailer2);
 		ListitemUnit u1 = new ListitemUnit();
-
 		Listitem listitem1 = new Listitem(2.0f, u1);
 		Listitem listitem2 = new Listitem(2.0f, u1);
 		Listitem listitem3 = new Listitem(2.0f, u1);
-//		Listitem listitem2 = new Listitem(0.5f, ListitemUnit.Kg, retailer2, product1);	
-//		Listitem listitem3 = new Listitem(5.0f, ListitemUnit.St, retailer2, product2);	
-//		Listitem listitem4 = new Listitem(1.0f, ListitemUnit.L, retailer1, product2);	
 		listitems.add(listitem1);
 		listitems.add(listitem2);
 		listitems.add(listitem3);
-//		listitems.add(listitem4);
 
 		table = new CellTable<Listitem>();
 
@@ -146,6 +164,10 @@ public class ShoppinglistCellTable extends VerticalPanel {
 		final MultiSelectionModel<Listitem> selectionModel = new MultiSelectionModel<Listitem>(Listitem.KEY_PROVIDER);
 		table.setSelectionModel(selectionModel, DefaultSelectionEventManager.<Listitem>createCheckboxManager());
 
+		/**
+		 * Add Check Column
+		 * 
+		 */
 		Column<Listitem, Boolean> checkColumn = new Column<Listitem, Boolean>(new CheckboxCell(true, false)) {
 
 			@Override
@@ -154,78 +176,15 @@ public class ShoppinglistCellTable extends VerticalPanel {
 
 			}
 		};
-
 		checkColumn.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
 		checkColumn.setCellStyleNames("columncheck");
-		table.addColumn(checkColumn, "Check");
 		table.setColumnWidth(checkColumn, 40, Unit.PX);
 
-
-	
-
 		/**
-		 * Retailer Name Column DoTo: Colum creation in on Success machen, arraylist mit
-		 * allen listitem objekten und dann for schleife geändert: gstvm zeile 147
-		 * RootPanel.get("main").add(shoppinglist/groupShowForm); shoppinglistcontent
-		 * z30 auskommentiert
+		 * Get Data from Listitem ArrayList and push into Column
+		 * 
 		 */
-
 		for (int i = 0; i < listitems.size(); i++) {
-
-			
-			/**
-			 * Unit Name Column
-			 * 
-			 */
-			shoppinglistAdministration.getListitemUnitOf(listitems.get(i), new AsyncCallback<ListitemUnit>() {
-
-				@Override
-				public void onFailure(Throwable caught) {
-					
-
-				}
-
-				@Override
-				public void onSuccess(ListitemUnit result) {
-					Column<Listitem, String> unitNameColumn = new Column<Listitem, String>((new EditTextCell())) {
-						@Override
-						public String getValue(Listitem object) {
-							
-							return unitName;
-						}
-					};
-					table.addColumn(unitNameColumn, "Einheit");
-				}
-
-			});
-		
-			/**
-			 * Amount Column
-			 * 
-			 */
-			shoppinglistAdministration.getAmountOf(listitems.get(i), new AsyncCallback<Float>() {
-
-				@Override
-				public void onFailure(Throwable caught) {
-					// TODO Auto-generated method stub
-
-				}
-
-				@Override
-				public void onSuccess(Float result) {
-					
-					NumberCell amountCell = new NumberCell();
-					Column<Listitem, Number> listItemAmountColumn = new Column<Listitem, Number>(amountCell) {
-						@Override
-						public Number getValue(Listitem object) {
-							return object.getAmount();
-						}
-					};
-					table.addColumn(listItemAmountColumn, "Menge");
-
-				}
-
-			});
 
 			/**
 			 * Product Name Column
@@ -235,24 +194,95 @@ public class ShoppinglistCellTable extends VerticalPanel {
 
 				@Override
 				public void onFailure(Throwable caught) {
-					productName = "setzen fehlgeschlagen";
+					Notification.show("Name des Produkts konnte nicht geladen werden");
 
 				}
 
 				@Override
 				public void onSuccess(String result) {
-					productName = result;
+					if (result != null) {
+						productName.setText(result);
+						Column<Listitem, String> productNameColumn = new Column<Listitem, String>((new TextCell())) {
+							public String getValue(Listitem object) {
 
-					Column<Listitem, String> productNameColumn = new Column<Listitem, String>((new TextCell())) {
-						public String getValue(Listitem object) {
+								return productName.getText();
 
-							return productName;
-						}
-					};
-					table.addColumn(productNameColumn, "Produkt");
+							}
+
+						};
+
+						productNameToDisplay = productNameColumn;
+					} else {
+						Notification.show("Name des Produkts konnte nicht geladen werden");
+					}
 
 				}
 
+			});
+
+			/**
+			 * Amount Column
+			 * 
+			 */
+			shoppinglistAdministration.getAmountOf(listitems.get(i), new AsyncCallback<Float>() {
+
+				@Override
+				public void onFailure(Throwable caught) {
+					Notification.show("Menge konnte nicht geladen werden");
+
+				}
+
+				@Override
+				public void onSuccess(Float result) {
+					if (result != null) {
+						amount = result;
+						NumberCell amountCell = new NumberCell();
+						Column<Listitem, Number> listItemAmountColumn = new Column<Listitem, Number>(amountCell) {
+							@Override
+							public Number getValue(Listitem object) {
+
+								return amount;
+							}
+						};
+						amountToDisplay = listItemAmountColumn;
+
+					} else {
+						Notification.show("Name des Produkts konnte nicht geladen werden");
+					}
+				}
+
+			});
+
+			/**
+			 * Unit Name Column
+			 * 
+			 */
+			shoppinglistAdministration.getListitemUnitOf(listitems.get(i), new AsyncCallback<ListitemUnit>() {
+
+				@Override
+				public void onFailure(Throwable caught) {
+					Notification.show("Einheit konnte nicht geladen werden");
+
+				}
+
+				@Override
+				public void onSuccess(ListitemUnit result) {
+					if (result != null) {
+						unitName.setText(result.getName());
+						Column<Listitem, String> unitNameColumn = new Column<Listitem, String>(new EditTextCell()) {
+							@Override
+							public String getValue(Listitem object) {
+
+								return unitName.getText();
+
+							}
+						};
+						unitNameToDisplay = unitNameColumn;
+
+					} else {
+						Notification.show("Name des Produkts konnte nicht geladen werden");
+					}
+				}
 			});
 
 			/**
@@ -263,36 +293,30 @@ public class ShoppinglistCellTable extends VerticalPanel {
 
 				@Override
 				public void onFailure(Throwable caught) {
-					// TODO Auto-generated method stub
+					Notification.show("Haendler konnte nicht geladen werden");
 
 				}
 
 				@Override
 				public void onSuccess(Retailer result) {
+					if (result != null) {
+						retailerName.setText(result.getName());
+						Column<Listitem, String> retailerNameColumn = new Column<Listitem, String>((new TextCell())) {
+							@Override
+							public String getValue(Listitem object) {
 
-					retailerName.setText(retailerName2.getText());
-
-					Column<Listitem, String> retailerNameColumn = new Column<Listitem, String>((new TextCell())) {
-						@Override
-						public String getValue(Listitem object) {
-
-							return retailerName.getText();
-						}
-					};
-					table.addColumn(retailerNameColumn, "Haendler");
-
-					if (table.getColumn(1).toString() == "Haendler") {
-						table.removeColumn(1);
+								return retailerName.getText();
+							}
+						};
+						retailerNameToDisplay = retailerNameColumn;
 
 					} else {
-						// table.addColumn(retailerNameColumn, "Haendler");
+						Notification.show("Name des Produkts konnte nicht geladen werden");
 					}
-
 				}
-
 			});
 
-		}
+		} // end of for-loop
 
 		/**
 		 * Clickable edit button containing an image
@@ -322,6 +346,20 @@ public class ShoppinglistCellTable extends VerticalPanel {
 				}
 			}
 		};
+
+		/**
+		 * Add Columns to CellTable
+		 * 
+		 */
+		table.addColumn(checkColumn, "Check");
+
+		table.addColumn(productNameToDisplay, "Produkt");
+
+		table.addColumn(amountToDisplay, "Menge");
+
+		table.addColumn(unitNameToDisplay, "Einheit");
+
+		table.addColumn(retailerNameToDisplay, "Haendler");
 
 		table.addColumn(imageColumn, "Edit");
 
