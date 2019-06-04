40f456f61d4288b8b27fe4f4f1011a0d17e522ad
TimBeutelspacher <tb125@hdm-stuttgart.de>
2019-06-04 09:57:28 +0200

    created class ReportGeneratorEntry in package .client

---
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/client/ReportGeneratorEntry.java
@@ -0,0 +1,15 @@
+package de.hdm.softwarepraktikum.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class ReportGeneratorEntry implements EntryPoint{
+
+	
+	
+	@Override
+	public void onModuleLoad() {
+
+		
+	}
+
+}
