e36e778b42916f06fb36d18fcbaf7ad452105bff
ElinaEisele <ee024@hdm-stuttgart.de>
2019-06-18 14:03:31 +0200

    Improve NavigatorPanel

--- SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/client/gui/NavigatorPanel.java
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/client/gui/NavigatorPanel.java
@@ -2,7 +2,10 @@ package de.hdm.softwarepraktikum.client.gui;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.logical.shared.CloseEvent;
+import com.google.gwt.event.logical.shared.CloseHandler;
 import com.google.gwt.user.cellview.client.CellTree;
+import com.google.gwt.user.cellview.client.TreeNode;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
@@ -11,6 +14,7 @@ import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.view.client.TreeViewModel.NodeInfo;
 
 import de.hdm.softwarepraktikum.client.ClientsideSettings;
 import de.hdm.softwarepraktikum.client.ShoppinglistEditorEntryLogin.CurrentUser;
@@ -139,15 +143,16 @@ public class NavigatorPanel extends VerticalPanel {
 				NewGroupForm ngf = new NewGroupForm();
 				if (selectedGroup != null) {
 					ngf.setOldSelectedGroup(selectedGroup);
-				}
-				GroupShowForm gsf = new GroupShowForm(ngf);
-				if (selectedGroup != null) {
+					GroupShowForm gsf = new GroupShowForm(ngf);
 					gsf.setSelected(selectedGroup);
+										
+
 				}
 
 			}
 		}
 
 	}
+
 
 }
