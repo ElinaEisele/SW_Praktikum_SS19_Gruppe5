4edc1c958c9d5fa96a74734052a67be39287042f
FelixRapp <fr056@hdm-stuttgart.de>
2019-06-20 11:08:03 +0200

    Changed Method getListitemsOf and added Method getAllListitemsOf

--- SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/shared/ShoppinglistAdministrationAsync.java
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/shared/ShoppinglistAdministrationAsync.java
@@ -81,7 +81,9 @@ public interface ShoppinglistAdministrationAsync {
 	
 	void getListitemsByNameOf(Shoppinglist shoppinglist, String productname, AsyncCallback<ArrayList<Listitem>>callback) throws IllegalArgumentException;
 
-	void getListitemsOf(Shoppinglist shoppinglist, AsyncCallback<ArrayList<Listitem>> callback);
+	void getListitemsOf(Shoppinglist shoppinglist, AsyncCallback<ArrayList<Listitem>> callback) throws IllegalArgumentException;
+	
+	void getAllListitemsOf(Shoppinglist shoppinglist, AsyncCallback<ArrayList<Listitem>> callback) throws IllegalArgumentException;
 	
 	void getAllRetailers(AsyncCallback<ArrayList<Retailer>> callback) throws IllegalArgumentException;
 	
