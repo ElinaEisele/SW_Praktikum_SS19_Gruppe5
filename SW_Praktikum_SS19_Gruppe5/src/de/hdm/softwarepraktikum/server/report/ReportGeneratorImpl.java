cfb59d9255746814142af11b9e22d578978c0248
CarlaHofmann <ch143@hdm-stuttgart.de>
2019-06-24 10:43:39 +0200

    Test sum archived Listitems

--- SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/server/report/ReportGeneratorImpl.java
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/server/report/ReportGeneratorImpl.java
@@ -217,8 +217,9 @@ public class ReportGeneratorImpl extends RemoteServiceServlet implements ReportG
     				sumListitems.remove(rl);
     				this.amount = sl.getAmount() + rl.getAmount();
     				sl.setAmount(amount);
-    				
-    			}
+//       			}else {
+//       				sumListitems.add(rl);
+       			}
     		}
     		
     	}
