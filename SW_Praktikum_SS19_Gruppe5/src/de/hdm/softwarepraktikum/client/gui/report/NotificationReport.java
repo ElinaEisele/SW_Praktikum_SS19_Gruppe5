b025128a4e1219c87c05cf275158c7e44fa95a34
CarlaHofmann <ch143@hdm-stuttgart.de>
2019-07-01 11:28:24 +0200

    Insert class NotificationReport

--- SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/client/gui/report/NotificationReport.java
+++ SW_Praktikum_SS19_Gruppe5/src/de/hdm/softwarepraktikum/client/gui/report/NotificationReport.java
@@ -8,21 +8,21 @@ import com.google.gwt.user.client.ui.RootPanel;
  * Die Klasse <code>Notification</code>, instanziiert neue Objekte einer Notification, welche während der 
  * Laufzeit im Header angezeigt werden. Diese Klasse kann störende Window-alerts ersetzen.
  * 
- * @author ElinaEisele, JonasWagenknecht
+ * @author CarlaHofmann
  *
  */
-public class Notification {
+public class NotificationReport {
 	
 	private static Label notificationLabel = new Label();
 	private static boolean isActive = false;
 	
-	public Notification() {
+	public NotificationReport() {
 		
 	}
 	
 	public static void clear() {
 		if (isActive == true) {
-			RootPanel.get("header").remove(RootPanel.get("header").getWidgetIndex(notificationLabel));
+			RootPanel.get("reportHeader").remove(RootPanel.get("reportHeader").getWidgetIndex(notificationLabel));
 		}
 	}
 	
@@ -34,13 +34,13 @@ public class Notification {
 	public static void show (String message) {
 		notificationLabel.setText(message);
 		notificationLabel.setStyleName("notificationLabel");
-		RootPanel.get("header").add(notificationLabel);
+		RootPanel.get("reportHeader").add(notificationLabel);
 		
 		final Timer timer = new Timer() {
 
 			@Override
 			public void run() {
-				RootPanel.get("header").remove(RootPanel.get("header").getWidgetIndex(notificationLabel));
+				RootPanel.get("reportHeader").remove(RootPanel.get("reportHeader").getWidgetIndex(notificationLabel));
 			}
 			
 		};
